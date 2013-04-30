0932aafc560046e98b2422f510a1914c09af9941
John Else <john.else@citrix.com>
2013-04-30 18:53:40 -0400

    Get dom0 ref from xapi
    Signed-off-by: John Else <john.else@citrix.com>

--- dd-test/dd_test.ml
+++ dd-test/dd_test.ml
@@ -1,4 +1,22 @@
 let _dd = "/bin/dd"
 let _sparse_dd = "/opt/xensource/libexec/sparse_dd"
 
-let () = ()
+let xapi_rpc request =
+	Rpc_client.do_rpc_unix
+		~content_type:(Rpc_client.content_type_of_string "text/xml")
+		~filename:(Filename.concat "/var/lib/xcp" "xapi")
+		~path:"/" request
+
+module XenAPI = Client.Client
+
+let get_dom0 ~rpc ~session_id =
+	Inventory.inventory_filename := "/etc/xensource-inventory";
+	let control_domain_uuid = Inventory.lookup Inventory._control_domain_uuid in
+	XenAPI.VM.get_by_uuid ~rpc ~session_id ~uuid:control_domain_uuid
+
+let () =
+	let rpc = xapi_rpc in
+	let session_id = XenAPI.Session.login_with_password
+		~rpc ~uname:"dd-test" ~pwd:"" ~version:"1.1" in
+	let dom0 = get_dom0 ~rpc ~session_id in
+	print_endline (API.Ref.of_string dom0)
