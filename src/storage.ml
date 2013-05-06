19545c8c78ba526f189e8428b26c02e5286c9035
John Else <john.else@citrix.com>
2013-05-06 09:20:44 +0100

    Fix get_vhd_path for file SRs
    Signed-off-by: John Else <john.else@citrix.com>

--- src/storage.ml
+++ src/storage.ml
@@ -7,7 +7,7 @@ let get_vhd_path ~rpc ~session_id ~sr ~vdi =
 	let sr_type = XenAPI.SR.get_type ~rpc ~session_id ~self:sr in
 	match sr_type with
 	| "nfs" | "ext" ->
-		Printf.sprintf "/var/run/sr-mount/%s/%s"
+		Printf.sprintf "/var/run/sr-mount/%s/%s.vhd"
 			(XenAPI.SR.get_uuid ~rpc ~session_id ~self:sr)
 			(XenAPI.VDI.get_uuid ~rpc ~session_id ~self:vdi)
 	| "lvm" | "lvmohba" | "lvmoiscsi" ->
