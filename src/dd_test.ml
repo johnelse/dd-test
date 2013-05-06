fbdb85219ca8d52c8451b0c2ca3dd95fa3fb3f11
John Else <john.else@citrix.com>
2013-05-06 07:48:19 +0100

    Try to find an existing inventory file
    Signed-off-by: John Else <john.else@citrix.com>

--- src/dd_test.ml
+++ src/dd_test.ml
@@ -23,8 +23,22 @@ let get_vhd_path ~rpc ~session_id ~sr ~vdi =
 	| _ ->
 		raise Sr_unsupported
 
+let rec find_existing_file ~files =
+	match files with
+	| [] -> raise Not_found
+	| f :: rest ->
+		if Sys.file_exists f
+		then f
+		else find_existing_file ~files:rest
+
+let possible_inventory_files = [
+	"/etc/xensource-inventory";
+	"/etc/xcp/inventory";
+]
+
 let get_dom0 ~rpc ~session_id =
-	Inventory.inventory_filename := "/etc/xensource-inventory";
+	Inventory.inventory_filename :=
+		find_existing_file ~files:possible_inventory_files;
 	let control_domain_uuid = Inventory.lookup Inventory._control_domain_uuid in
 	XenAPI.VM.get_by_uuid ~rpc ~session_id ~uuid:control_domain_uuid
 
