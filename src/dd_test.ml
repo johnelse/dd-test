8a411dd4e1191f4dd45692b7126a181931b74c5e
John Else <john.else@citrix.com>
2013-05-06 06:25:19 +0100

    Add function get_vhd_path
    Given an SR and VDI reference, returns the dom0 path to the
    corresponding VHD file.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- src/dd_test.ml
+++ src/dd_test.ml
@@ -7,6 +7,18 @@ let xapi_rpc request =
 open API
 module XenAPI = Client.Client
 
+exception Sr_unsupported
+
+let get_vhd_path ~rpc ~session_id ~sr ~vdi =
+	let sr_type = XenAPI.SR.get_type ~rpc ~session_id ~self:sr in
+	match sr_type with
+	| "nfs" | "ext" ->
+		Printf.sprintf "/var/run/sr-mount/%s/%s"
+			(XenAPI.SR.get_uuid ~rpc ~session_id ~self:sr)
+			(XenAPI.VDI.get_uuid ~rpc ~session_id ~self:vdi)
+	| _ ->
+		raise Sr_unsupported
+
 let get_dom0 ~rpc ~session_id =
 	Inventory.inventory_filename := "/etc/xensource-inventory";
 	let control_domain_uuid = Inventory.lookup Inventory._control_domain_uuid in
