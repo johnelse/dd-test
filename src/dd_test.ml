fbf1f435a8be15ffd382990fcad3e426efedddd6
John Else <john.else@citrix.com>
2013-05-06 06:53:02 +0100

    Add lvm/lvmohba/lvmoiscsi support
    Signed-off-by: John Else <john.else@citrix.com>

--- src/dd_test.ml
+++ src/dd_test.ml
@@ -16,6 +16,10 @@ let get_vhd_path ~rpc ~session_id ~sr ~vdi =
 		Printf.sprintf "/var/run/sr-mount/%s/%s"
 			(XenAPI.SR.get_uuid ~rpc ~session_id ~self:sr)
 			(XenAPI.VDI.get_uuid ~rpc ~session_id ~self:vdi)
+	| "lvm" | "lvmohba" | "lvmoiscsi" ->
+		Printf.sprintf "/dev/sm/backend/%s/%s"
+			(XenAPI.SR.get_uuid ~rpc ~session_id ~self:sr)
+			(XenAPI.VDI.get_uuid ~rpc ~session_id ~self:vdi)
 	| _ ->
 		raise Sr_unsupported
 
